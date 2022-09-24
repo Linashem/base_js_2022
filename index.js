06505d962be01824fa9d2c027f7d942157a52813
Linashem <lina.shemeneva@yandex.ru>
2022-09-24 12:43:50 +0300

    lesson2409

---
+++ index.js
@@ -0,0 +1,19 @@
+// const name = 'alex';
+// const age = 18;
+//  const userData = `имя пользователя: ${name}. возраст ${age}`;
+//  console.log(userData);
+
+//  const isUser = false;
+//  const isVisible = true;
+
+//  let townName;
+//  console.log(townName);
+//  townName = 'moscow'
+
+//  console.log(townName);
+
+const userData = {
+  name: "Ivan",
+  age: "34"
+};
+console.log(typeof userData);
