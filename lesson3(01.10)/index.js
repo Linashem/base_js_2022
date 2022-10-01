933858f9037fd8b8838587176819ae1c22dc2621
Lina <103552144+Linashem@users.noreply.github.com>
2022-10-01 12:42:45 +0300

    Add files via upload

---
+++ lesson3(01.10)/index.js
@@ -0,0 +1,89 @@
+// const name = 'alex';
+
+
+// const namberName = Number(name);
+
+// console.log(namberName);
+
+// const age = 26;
+
+// const ageToString = String(age);
+// console.log(typeof ageToString);
+
+// const nullValue = null;
+// console.log(Number(nullValue));
+
+// console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean({}));
+
+
+
+
+// console.log(typeof+'45');
+
+// console.log(typeof Number('45'));
+
+
+
+
+// console.log(2==3);
+// console.log(2==2);
+// console.log(2!==2);
+
+
+// console.log(typeof('3'*"2"));
+
+// console.log(2===2);
+
+
+// console.log('2'>'12');
+// console.log(5>4);
+// console.log(undefined==null);
+// console.log(undefined===null);
+// console.log(null=='\n0\n');
+// console.log(null=== +'\n0\n');
+// console.log(''+1+0);
+// console.log(''-1+0);
+// console.log(true + false);
+
+
+// console.log(4+5+'px');
+// console.log('$'+4+5);
+// console.log('4'-2);
+// console.log('4px'-2);
+// console.log(7/0);
+// console.log(' -9 '+5);
+// console.log('-9'-5);
+// console.log(null+1);
+// console.log(undefined +1);
+// console.log('\t\n'-2);
+
+
+// const towns = ['Moscow', 'Paris', 'London', 'NN'];
+// console.log(typeof{});
+// console.log(typeof[]);
+
+// console.log(towns);
+// towns[1]= "Milan";
+// console.log(towns);
+
+// console.log(towns[0]);
+
+
+// towns[4] = 'St,Peterburg';
+// console.log(towns);
+
+// towns[9] = 'Berlin';
+// console.log(towns);
+// towns.push("Berlin");
+// towns[towns.length] = 'Orel';
+// console.log(towns);
+// console.log(towns.length);
+
+const films = ['Narkoz', '13 sins', 'Friends'];
+console.log(films[0]);
+films.push('Vasabi');
+console.log(films)
+console.log(films[films.length-1]);
