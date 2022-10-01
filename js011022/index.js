bcb599af8368f0be992402c41daadb3b7e0a421b
Lina <103552144+Linashem@users.noreply.github.com>
2022-10-01 11:38:52 +0300

    Add files via upload

---
+++ js011022/index.js
@@ -0,0 +1,61 @@
+// const name = 'alex';
+
+
+// const namberName = Number(name);
+
+// console.log(namberName);
+
+// const age = 26;
+
+// const ageToString = String(age);
+// console.log(typeof ageToString);
+
+// const nullValue = null;
+// console.log(Number(nullValue));
+
+// console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean({}));
+
+
+
+
+// console.log(typeof+'45');
+
+// console.log(typeof Number('45'));
+
+
+
+
+// console.log(2==3);
+// console.log(2==2);
+// console.log(2!==2);
+
+
+// console.log(typeof('3'*"2"));
+
+// console.log(2===2);
+
+
+console.log('2'>'12');
+console.log(5>4);
+console.log(undefined==null);
+console.log(undefined===null);
+console.log(null=='\n0\n');
+console.log(null=== +'\n0\n');
+console.log(''+1+0);
+console.log(''-1+0);
+console.log(true + false);
+
+
+console.log(4+5+'px');
+console.log('$'+4+5);
+console.log('4'-2);
+console.log('4px'-2);
+console.log(7/0);
+console.log(' -9 '+5);
+console.log('-9'-5);
+console.log(null+1);
+console.log(undefined +1);
+console.log('\t\n'-2);
