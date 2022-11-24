31a7c5ac1dc8c2309d8a71fab5ebc4842a63b1e3
Lina <103552144+Linashem@users.noreply.github.com>
2022-11-24 15:52:22 +0300

    Add files via upload

---
+++ class-work/lesson10(19.11)/index.js
@@ -0,0 +1,154 @@
+
+//Замыкание Практика 1
+let  cubeValue=(a) =>{
+    return(b,c)=>{
+        return a*b*c
+    }
+}
+const first = cubeValue(10);
+console.log(first(10, 10));
+console.log(first(2, 5));
+console.log(first(5, 7));
+
+
+const second = cubeValue(4);
+console.log(second(10, 10));
+console.log(second(2, 5));
+console.log(second(10, 10));
+
+
+
+//Каррирование Практика 2
+const cubeVal=(a,b,c)=>a*b*c;
+console.log(cubeVal(1,3,3));
+
+const cubeCarryValue=(a)=>{
+    return (b)=>{
+        return (c)=>{
+            return a*b*c;
+        }
+    }
+}
+
+//Краткая версия cubeCarryValue
+const cubeCarryValue2 =(a)=>(b)=>(c)=>a*b*c;
+
+
+console.log(cubeCarryValue(1)(3)(3));
+
+const twoArg =cubeCarryValue(1)(3);
+console.log(twoArg(3));
+
+
+
+
+//Протатипы [[Prototype]]
+const russian ={
+    country:'Russia',
+    city:"NN",
+}
+const people = {
+    name:'Alex',
+    age:26,
+    __proto__:russian
+    }
+console.log(people.city, people);
+
+
+// Функции конструкторы
+function CreateUser(userName, userAge){
+    this.name =userName;
+    this.age = userAge;
+    this.sayHi=function(){
+        console.log(`Hello, my name is ${this.name}`);
+    }
+}
+
+CreateUser.prototype.sayAge = function(){
+    console.log(`my age is ${this.age}`);
+}
+
+const firstUser = new CreateUser('Alex', 26);
+const secondUser = new CreateUser('Tony', 24);
+const firdUser = new CreateUser('Ivan', 15);
+
+console.log(firstUser, secondUser, firdUser);
+console.log(firstUser);
+console.log(secondUser);
+console.log(firdUser);
+
+firdUser.sayHi()
+secondUser.sayAge()
+
+
+//Практика 
+function CreatPostcard(nameFrom, NameTo) {
+    this.from=nameFrom; 
+    this.to = NameTo;
+    this.show= function(){
+        console.log(`This postcard from ${this.from} to ${this.to}`);
+    }
+}
+
+const firstPerson = new CreatPostcard('Kate', 'Anna');
+firstPerson.show();
+
+CreatPostcard.prototype.change = function(from){
+this.from =from
+}
+
+firstPerson.change('Ivan')
+firstPerson.show();
+
+
+
+class People {
+    constructor(name, surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    getFullName(){
+        return `${this.name} ${this.surname}`
+    }
+}
+
+const firstPeople = new People('Alex', 'Ivanov');
+
+console.log(firstPeople.getFullName());
+
+
+
+
+//Практика с занития на КЛАССЫ
+class People{
+    constructor(name, surName){
+        this.name = name;
+        this.surName = surName
+    }
+    getFullName (){
+        return `${this.name} :  ${this.surName}`
+    }
+}
+
+// const firstPeople =new People('Alex', 'Ivanov')
+// console.log(firstPeople.getFullName());
+
+class Worker extends People{
+    constructor(name, surName, rate, day){
+        super (name, surName);
+        this.rate = rate;
+        this.day = day;
+    }
+    getSallary (){
+        return `${this.getFullName()} takes ${this.rate * this.day}`
+    }
+}
+const firstWorker = new Worker('Alex', 'Petrov', 500, 20);
+
+const secondWorker = new Worker('Tony', 'Ivanov', 700, 25)
+
+console.log(firstWorker.getFullName());
+console.log(firstWorker.getSallary());
+
+console.log(secondWorker.getFullName());
+console.log(secondWorker.getSallary());
