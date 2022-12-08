0d13a1988c70d5418418ed006335430650980195
Lina <103552144+Linashem@users.noreply.github.com>
2022-12-08 13:05:54 +0300

    Add files via upload

---
+++ class-work/lesson12(02.12)/index.js
@@ -0,0 +1,16 @@
+// function greet() {
+//     console.log("Hello!");
+//    }
+//    function delay() {
+//     for (let i = 0; i < 10000000000; i += 1) {}
+//    }
+   
+//    delay();
+//    greet();
+function greet() {
+    console.log("Hello!");
+   }
+   
+   setTimeout(greet, 5000);
+   
+   console.log("I am working!");
